022600bfbb5cba53969fc7fefb512586be8a9325
Limofeus <69484438+Limofeus@users.noreply.github.com>
2024-04-29 22:17:55 +0300

    ClonningAltar#4: Выбор предмета для клонирования вроде работает норм

--- Assets/Scripts/CloningAltar.cs
+++ Assets/Scripts/CloningAltar.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class CloningAltar : MonoBehaviour
 {
+    [SerializeField] private GeneralizedItemIcon onAltarItem;
     [SerializeField] private GeneralizedItemIcon[] playerItemIcons;
     [SerializeField] private CloneAltarButton cloneAltarButton;
-    private int selectedItemId;
+    private int selectedItemSlot = 0; // (STARTS FROM 1!!) 0 - none, 1 - 1, 2 - 2, etc.
+    private int selectedItemId = -1;
     public SaveData saveData;
     void Start()
     {
@@ -15,6 +17,9 @@ public class CloningAltar : MonoBehaviour
             saveData = new SaveData(MenuDataManager.saveData);
         else
             saveData = SaveSystem.Load();
+
+        onAltarItem.HideShowItem(false);
+        UpdateItemsFromSaveData();
 
         playerItemIcons[0].UpdateItem(saveData.item1Id, 0, "ЛКМ - выбрать");
     }
@@ -25,9 +30,96 @@ public class CloningAltar : MonoBehaviour
         cloneAltarButton.UpdateCloneButton(saveData.item1Id, "222");
         cloneAltarButton.ChangeMode(CloneAltarButton.InteractMode.Normal);
     }
-
-    void Update()
+    public void TrySelectItem(int itemSlot)
     {
-        
+        if (selectedItemSlot == 0)
+        {
+            SelectItem(itemSlot);
+        }
+        else
+        {
+            if(selectedItemSlot == itemSlot)
+            {
+                TryDeselectItem();
+            }
+            else
+            {
+                TryDeselectItem();
+                SelectItem(itemSlot);
+            }
+        }
+        UpdateCloneButton();
+    }
+    private void SelectItem(int itemSlot)
+    {
+        switch (itemSlot)
+        {
+            case 1:
+                selectedItemId = saveData.item1Id;
+                break;
+            case 2:
+                selectedItemId = saveData.item2Id;
+                break;
+            case 3:
+                selectedItemId = saveData.item3Id;
+                break;
+        }
+
+        if (selectedItemId == -1) return;
+
+        onAltarItem.UpdateItem(selectedItemId);
+        onAltarItem.HideShowItem();
+        playerItemIcons[itemSlot - 1].HideShowItem(false);
+        selectedItemSlot = itemSlot;
+    }
+    private void UpdateCloneButton()
+    {
+        if(selectedItemId >= 0)
+        {
+            //CHANGE PRICE FUNTION
+            int clonePrice = 200 *
+            (ItemSpriteDictionary.itemRarity[selectedItemId] + 1);
+            cloneAltarButton.UpdateCloneButton(selectedItemId, clonePrice.ToString());
+            if(clonePrice <= saveData.moneyAmount)
+            {
+                cloneAltarButton.ChangeMode(CloneAltarButton.InteractMode.Normal);
+            }
+            else
+            {
+                cloneAltarButton.ChangeMode(CloneAltarButton.InteractMode.Locked);
+            }
+        }
+        else
+        {
+            cloneAltarButton.ChangeMode(CloneAltarButton.InteractMode.Hidden);
+        }
+    }
+    public void TryDeselectItem()
+    {
+        if(selectedItemSlot != 0)
+        {
+            onAltarItem.HideShowItem(false);
+            playerItemIcons[selectedItemSlot - 1].HideShowItem();
+            selectedItemSlot = 0;
+            selectedItemId = -1;
+        }
+        UpdateCloneButton();
+    }
+    private void UpdateItemsFromSaveData()
+    {
+        if (saveData.item1Id >= 0)
+            playerItemIcons[0].UpdateItem(saveData.item1Id, 0, "ЛКМ - выбрать");
+        else
+            playerItemIcons[0].gameObject.SetActive(false);
+
+        if (saveData.item2Id >= 0)
+            playerItemIcons[1].UpdateItem(saveData.item2Id, 1, "ЛКМ - выбрать");
+        else
+            playerItemIcons[1].gameObject.SetActive(false);
+
+        if (saveData.item3Id >= 0)
+            playerItemIcons[2].UpdateItem(saveData.item3Id, 2, "ЛКМ - выбрать");
+        else
+            playerItemIcons[2].gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/UiScripts/CloneAltarButton.cs
+++ Assets/Scripts/UiScripts/CloneAltarButton.cs
@@ -9,10 +9,12 @@ public class CloneAltarButton : MonoBehaviour
     public enum InteractMode {Hidden, Locked, Normal}
 
     [SerializeField] private Transform scalingTransform;
+    [SerializeField] private Transform priceHolderTransform;
     [SerializeField] private float lerpPower;
 
     [SerializeField] private Color unselectedColor;
     [SerializeField] private Color selectedColor;
+    [SerializeField] private Color lockedColor;
 
     [SerializeField] private Color[] rarityColours;
     [SerializeField] private SpriteRenderer sprite;
@@ -27,27 +29,44 @@ public class CloneAltarButton : MonoBehaviour
 
     private bool mouseOver;
 
+    private void Start()
+    {
+        scalingTransform.localScale = Vector3.zero;
+        priceHolderTransform.localScale = Vector3.zero;
+    }
+
     private void Update()
     {
         if(interactMode == InteractMode.Hidden)
         {
-            LerpScaleAndColor(Vector3.zero, new Color(1f, 1f, 1f, 0f));
+            LerpScaleAndColor(Vector3.zero, new Color(1f, 1f, 1f, 0f), 0f);
         }
         else
         {
             if(mouseOver && interactMode == InteractMode.Normal)
             {
-                LerpScaleAndColor(Vector3.one * unselectedSelectedScale.y, selectedColor);
+                LerpScaleAndColor(Vector3.one * unselectedSelectedScale.y, selectedColor, 1f);
             }
             else
             {
-                LerpScaleAndColor(Vector3.one * unselectedSelectedScale.x, unselectedColor);
+                if(interactMode == InteractMode.Locked)
+                {
+                    LerpScaleAndColor(Vector3.one * unselectedSelectedScale.x, lockedColor, 1f);
+                }
+                else
+                {
+                    LerpScaleAndColor(Vector3.one * unselectedSelectedScale.x, unselectedColor, 1f);
+                }
             }
         }
     }
 
     public void ChangeMode(InteractMode newMode)
     {
+        if(newMode == InteractMode.Hidden)
+        {
+
+        }
         interactMode = newMode;
     }
     public void UpdateCloneButton(int itemId, string cloneCost = "?")
@@ -58,12 +77,12 @@ public class CloneAltarButton : MonoBehaviour
         costText.text = cloneCost;
     }
 
-    private void LerpScaleAndColor(Vector3 desiredScale, Color desiredColor)
+    private void LerpScaleAndColor(Vector3 desiredScale, Color desiredColor, float priceDesiredScale)
     {
         scalingTransform.localScale = Vector3.Lerp(scalingTransform.localScale, desiredScale, 1f - Mathf.Exp(-lerpPower * Time.deltaTime));
+        priceHolderTransform.localScale = Vector3.Lerp(priceHolderTransform.localScale, priceDesiredScale * Vector3.one, 1f - Mathf.Exp(-lerpPower * Time.deltaTime));
         sprite.color = Color.Lerp(sprite.color, desiredColor, 1f - Mathf.Exp(-lerpPower * Time.deltaTime));
         background.color = Color.Lerp(background.color, desiredColor * targetBgColor, 1f - Mathf.Exp(-lerpPower * Time.deltaTime));
-
     }
 
     private void OnMouseEnter()
