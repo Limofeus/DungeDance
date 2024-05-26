bfef10ce271bb01893c46b25d851c14f46f58ffa
Limofeus <69484438+Limofeus@users.noreply.github.com>
2024-05-27 00:37:38 +0300

    ClonningAltar#11: Выход к карте, плавно затемнение при входе/выходе

---
+++ Assets/Scripts/QuickSceneChange.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuickSceneChange : MonoBehaviour
+{
+    [SerializeField] private int defaultSceneId;
+    [SerializeField] private bool preloadScene;
+    [SerializeField] private Animator blackScreenAnimator; 
+    private AsyncOperation asyncLoad;
+
+
+    private void Start()
+    {
+        if (preloadScene)
+        {
+            asyncLoad = SceneManager.LoadSceneAsync(defaultSceneId);
+            asyncLoad.allowSceneActivation = false;
+        }
+    }
+    public void ChangeScene()
+    {
+        ChangeSceneById(-1);
+    }
+    public void ChangeSceneById(int sceneId)
+    {
+        if (sceneId < 0) sceneId = defaultSceneId;
+        if (preloadScene)
+        {
+            StartCoroutine(AnimateAndExit());
+        }
+    }
+
+    IEnumerator AnimateAndExit()
+    {
+        blackScreenAnimator.SetTrigger("Fade");
+        yield return new WaitForSeconds(0.45f);
+        asyncLoad.allowSceneActivation = true;
+    }
+}
